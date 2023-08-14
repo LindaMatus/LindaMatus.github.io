54e2a47ac1b1647ee95c857abcfa08174e187c13
LindaMatus <141804465+LindaMatus@users.noreply.github.com>
2023-08-14 13:43:38 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('.nav__menu');
+const menulist = document.querySelector('.nav__list');
+const links = document.querySelectorAll('.nav__link');
+
+menu.addEventListener('click', function(){
+    menulist.classList.toggle('nav__list--show');
+});
+
+links.forEach(function(link){
+
+    link.addEventListener('clic', function(){
+
+        menulist.classList.remove('nav__list--show');
+
+    })
+
+});
+
